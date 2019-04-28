d310ddaafb23fe3a100d62d9a4321a9e0b728a78
Lars Janssen <larsjanssen64@gmail.com>
2019-04-28 11:02:28 +0200

    A new game can be created (modal)

---
+++ frontend/components/modals/Game/NewGameModal.vue
@@ -0,0 +1,59 @@
+<template>
+    <modal name="new-game-modal"
+           :adaptive="true"
+           :scrollable="false"
+           :max-width="maxWidth"
+           height="auto">
+
+        <form id="scroll" class="overflow-y-auto p-8 center w-full h-full" @submit.prevent="create()">
+            <div class="center w-full flex-col">
+                <h2 class="text-black text-center text-2xl">Nieuwe game</h2>
+
+                <div class="w-full">
+                    <div class="flex flex-col p-4">
+                        <input class="mt-4 bg-white appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple"
+                               v-model="form.name"
+                               name="name"
+                               id="name"
+                               type="text"
+                               placeholder="Naam">
+
+                        <textarea class="mt-4 shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+                               v-model="form.description"
+                               name="description"
+                               id="description"
+                               placeholder="Omschrijving"></textarea>
+
+
+                        <button class="bg-grey-darker mt-4 hover:bg-grey text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            Opslaan
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </modal>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                maxWidth: 420,
+                form: {
+                    name: '',
+                    description: ''
+                }
+            }
+        },
+
+        methods: {
+            create() {
+
+                axios.post('/1/api/game', this.form).then((response) => {
+                    window.location.reload();
+                });
+            }
+        }
+    }
+</script>
