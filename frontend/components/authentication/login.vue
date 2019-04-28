29f8f304bc410403cc94851b99459f6aba7e6500
Lars Janssen <larsjanssen64@gmail.com>
2019-04-28 11:02:04 +0200

    Vue.js authentication components

---
+++ frontend/components/authentication/login.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="flex flex-wrap justify-center items-center w-full h-full px-64 mt-8">
+        <div class="flex w-full mt-12">
+            <form @submit.prevent="login()" class="bg-white w-full shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <h2 class="mb-4">Login</h2>
+
+                <div class="mb-4">
+                    <label class="block text-grey-darker text-sm font-bold mb-2" for="email2">
+                        Email
+                    </label>
+
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+                           id="email2"
+                           type="email"
+                           v-model="form.email"
+                           placeholder="Email"
+                           required>
+                </div>
+
+                <div class="mb-6">
+                    <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+
+                    <input class="shadow appearance-none rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                           id="password"
+                           type="password"
+                           v-model="form.password"
+                           placeholder="******************"
+                           required>
+                </div>
+
+                <div class="flex items-center">
+                    <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="submit">
+                        Login
+                    </button>
+
+                    <div class="cursor-pointer ml-2 underline" @click="register()">
+                        Registreer hier
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from '../../../frontend/axios';
+
+    export default {
+        data() {
+            return {
+                form: {
+                    email: null,
+                    password: null,
+                }
+            }
+        },
+
+        methods: {
+            login() {
+                axios.post('/api/jwt/login', this.form).then(({data}) => {
+                    localStorage.setItem('token', data);
+                    this.$router.push({name:'games'})
+                }).catch((error) => {
+                    alert('Wrong credentials!');
+                });
+            },
+
+            register() {
+                this.$router.push({name:'register'})
+            }
+        }
+    }
+</script>
---
+++ frontend/components/authentication/logout.vue
@@ -0,0 +1,17 @@
+<template>
+    <button @click="logout" class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        Logout
+    </button>
+</template>
+
+<script>
+    export default {
+        methods: {
+            logout() {
+                axios.get('/1/api/jxrs/logout').then((response) => {
+                    window.location.href = '/1/session-authentication';
+                })
+            }
+        }
+    }
+</script>
